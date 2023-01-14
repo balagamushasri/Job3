860d9f75f3037621e1f2bfb3aac404423d509a6b
ushasri <ushasribalagam@gmail.com>
2023-01-14 11:13:30 +0530

    first commit

---
+++ Hellojenkins.java
@@ -0,0 +1,11 @@
+package Package3;
+
+public class Hellojenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Jenkins");
+
+	}
+
+}
